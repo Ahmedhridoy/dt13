3f2e34cbe15a97dd2bad8077ba57d02f141af65a
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-09-12 14:07:18 +0600

    temp : trigger rice

--- product_config.mk
+++ product_config.mk
@@ -1,4 +1,4 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_lavender.mk
+    $(LOCAL_DIR)/lineage_lavender.mk
 
-COMMON_LUNCH_CHOICES := derp_lavender-userdebug
+COMMON_LUNCH_CHOICES := lineage_lavender-userdebug
